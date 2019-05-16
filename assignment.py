6ced1f7f15939bec543ebc2133ec8a46e5943b42
Zhangmaoshen <44254953+Zhangmaoshen@users.noreply.github.com>
2019-05-16 16:32:46 +0800

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+################################
+#
+'''
+read two books from the reference
+
+
+Preparation of Project-01
+Using python Flask or Bottle to finish your first simple web app:
+https://bottlepy.org/
+
+Learn what's the SQL, and try some simple SQL operations:
+https://www.w3schools.com/sql/sql_intro.asp
+
+Learn what's the HTML ( ONLY need to know the basic things)
+https://getbootstrap.com/; https://www.w3schools.com/html/
+'''
